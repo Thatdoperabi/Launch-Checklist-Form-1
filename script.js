3887220fa4ce96fa8b371e09af17bad0cd30760e
Thatdoperabi <Thatdoperabi@gmailcom>
2021-04-03 17:29:33 -0500

    finished

--- script.js
+++ script.js
@@ -1,13 +1,105 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+   const json = fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+      response.json().then( function(json) {
+         
+         
+         const div = document.getElementById("missionTarget");
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[3].name}</li>
+            <li>Diameter: ${json[3].diameter}</li>
+            <li>Star: ${json[3].star}</li>
+            <li>Distance from Earth: ${json[3].distance}</li>
+            <li>Number of Moons: ${json[3].moons}</li>
+         </ol>
+         <img src="${"https://mars.nasa.gov/system/resources/detail_files/7808_global-color-views-mars-PIA00407-full2.jpg"}">
+         `;
+      })
+   })
+
+   for (const input of document.getElementsByClassName('letters-only')) {
+      input.addEventListener('keypress', (event) => {
+        if (!/[a-zA-Z]/.test(event.key)) {
+          event.preventDefault();
+        }
+      })
+   }
+   
+
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      let pilotNameInput = document.querySelector("input[name=pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+      fuelLevelNumber = Number(fuelLevelInput.value)
+      cargoMassNumber = Number(cargoMassInput.value)
+
+   
+    
+    
+   
+
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required");
+
+         
+      }
+
+      document.getElementById("pilotStatus").innerHTML = `${pilotNameInput.value}` + " is ready";
+      document.getElementById("copilotStatus").innerHTML = `${copilotNameInput.value}` + " is ready";
+
+      let faultyItemsList = document.getElementById("faultyItems")
+      let launchStatusChecker = document.getElementById("launchStatus")
+      let fuelLevelChecker = document.getElementById("fuelStatus")
+      let cargoMassChecker = document.getElementById("cargoStatus")
+     
+     //both false
+      if (fuelLevelNumber < 10000 && cargoMassNumber > 10000) {
+         faultyItemsList.style.visibility = "visible"
+         fuelLevelChecker.innerHTML = "not enough fuel for the journey."
+         cargoMassChecker.innerHTML = "there is too much mass for the shuttle to take off."
+         launchStatusChecker.style.color = "red";
+         launchStatusChecker.innerHTML = "Shuttle not ready for launch"
+      }
+         else if (fuelLevelNumber < 10000 && cargoMassNumber < 10000) {
+            faultyItemsList.style.visibility = "visible"
+            fuelLevelChecker.innerHTML = "not enough fuel for the journey."
+            launchStatusChecker.style.color = "red";
+            launchStatusChecker.innerHTML = "Shuttle not ready for launch"
+            cargoMassChecker.innerHTML = "Cargo is good."
+         }
+         
+         else if (fuelLevelNumber > 10000 && cargoMassNumber > 10000) {
+            faultyItemsList.style.visibility = "visible"
+            cargoMassChecker.innerHTML = "there is too much mass for the shuttle to take off."
+            launchStatusChecker.style.color = "red";
+            launchStatusChecker.innerHTML = "Shuttle not ready for launch"
+            fuelLevelChecker.innerHTML = "Fuel level is good."
+         }
+
+            else {
+               cargoMassChecker.innerHTML = "Cargo is good."
+               fuelLevelChecker.innerHTML = "Fuel level is good."
+               launchStatusChecker.innerHTML = "Shuttle is ready for launch"
+               launchStatusChecker.style.color = "green";
+            }
+
+         
+
+
+         //else both is true
+         
+      
+
+  
+
+   });
+   
+
+
+});
